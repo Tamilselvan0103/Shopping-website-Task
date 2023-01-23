b5bb4a223ca7d376431ef2630c5cbb0a54857138
Tamilselvan0103 <tamilselvans0103@gmail.com>
2023-01-23 11:40:57 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -22,6 +22,7 @@ function App() {
       name: "Special Item",
       price: "18.00",
       pricesymbol: "$",
+      star: "⭐⭐⭐⭐⭐",
       backslash: "-",
       oldprice: "20.00",
     },
@@ -36,6 +37,7 @@ function App() {
     {
       img: "https://news.maxabout.com/wp-content/uploads/2021/09/Apple-iPhone-13-Mini-India-jpg-1-1536x1074.jpg",
       name: "Popular Item",
+      star: "⭐⭐⭐⭐⭐",
       oldprice: "$40.00",
     },
     {
@@ -57,6 +59,7 @@ function App() {
     {
       img: "https://news.maxabout.com/wp-content/uploads/2021/09/Apple-iPhone-13-Mini-India-jpg-1-1536x1074.jpg",
       name: "Special Item",
+      star: "⭐⭐⭐⭐⭐",
       price: "18.00",
       pricesymbol: "$",
       backslash: "-",
@@ -65,51 +68,44 @@ function App() {
     {
       img: "https://news.maxabout.com/wp-content/uploads/2021/09/Apple-iPhone-13-Mini-India-jpg-1-1536x1074.jpg",
       name: "Popular Item",
+      star: "⭐⭐⭐⭐⭐",
       oldprice: "$40.00",
     },
   ];
 
-  let [cart, setcart] = useState("0")
-
+  let [Cart, setCart] = useState(0);
   return (
     <div className="App">
-    
-      <NavDetails 
-       setcart = {setcart}
-      />
+      <NavDetails Cart={Cart} />
       <div className="container">
-      {data.map((iteam,idx)=>
-      (
-        <CartDetails
-        img = {iteam.img}
-        name = {iteam.name}
-        price = {iteam.price}
-        pricesymbol = {iteam.pricesymbol}
-        oldprice = {iteam.oldprice}
-        id = {idx}
-        backslash = {iteam.backslash}
-        setcart = {setcart}
-        />
-      )
-      
-      )}
+        {data.map((iteam, idx) => (
+          <CartDetails
+            img={iteam.img}
+            name={iteam.name}
+            price={iteam.price}
+            pricesymbol={iteam.pricesymbol}
+            oldprice={iteam.oldprice}
+            id={idx}
+            backslash={iteam.backslash}
+            star={iteam.star}
+            setCart={setCart}
+            Cart={Cart}
+            key={idx}
+          />
+        ))}
       </div>
-      <div className="footer">
-      Copyright © Your Website 2022
-      </div>
+      <div className="footer">Copyright © Your Website 2022</div>
     </div>
-  
   );
 }
 
 export default App;
 
 function NavDetails(props) {
-  
   return (
-    <div id ="key">
+    <div id="key">
       <section>
-        <Navbar bg="light" className="nav" expand="lg">
+        <Navbar bg="light" className="nav" expand="lg" key="id">
           <Container fluid>
             <Navbar.Brand href="#">Start Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
@@ -133,7 +129,10 @@ function NavDetails(props) {
                   </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-              <div className="cart"> 🛒 Cart<span className="cartvalue">{props.setcart}</span></div>
+              <div className="cart">
+                {" "}
+                🛒 Cart<span className="cartvalue">{props.Cart}</span>
+              </div>
             </Navbar.Collapse>
           </Container>
         </Navbar>
@@ -148,35 +147,45 @@ function NavDetails(props) {
   );
 }
 
-
-
-
 function CartDetails(props) {
-
-  
   let [Count, setCount] = useState(true);
-  let Showclick = ()=>{
-   setCount(!Count);
-   cart++
-   console.log(cart)
-
-  }
-  let RemoveClick = ()=>{
+  let Showclick = ({ setCart, Cart }) => {
     setCount(!Count);
-    cart--
-    console.log(cart)
-  }
+    setCart(Cart + 1);
+    console.log(Cart);
+  };
+  let RemoveClick = ({ setCart, Cart }) => {
+    setCount(!Count);
+    setCart(Cart - 1);
+    console.log(Cart);
+  };
   return (
-            <Card style={{ width: "18rem" }} id = {props.id} className = "card">
-              <Card.Img variant="top" src={props.img} />
-              <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
-                <Card.Text>
-                  <p>{props.star}</p>
-                  <p><span>{props.pricesymbol}{props.price}</span>{props.backslash}{props.pricesymbol}{props.oldprice}</p>
-                </Card.Text>
-                {Count ? <button className="btn1" onClick={Showclick}>Add to Cart</button> : <button className="btn1" onClick={RemoveClick}>Remove from Cart</button>}
-              </Card.Body>
-            </Card>
+    <Card style={{ width: "18rem" }} key={props.idx} className="card">
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          <p>{props.star}</p>
+          <p>
+            <span>
+              {props.pricesymbol}
+              {props.price}
+            </span>
+            {props.backslash}
+            {props.pricesymbol}
+            {props.oldprice}
+          </p>
+        </Card.Text>
+      </Card.Body>
+      {Count ? (
+        <button className="btn1" onClick={() => Showclick(props)}>
+          Add to Cart
+        </button>
+      ) : (
+        <button className="btn1" onClick={() => RemoveClick(props)}>
+          Remove from Cart
+        </button>
+      )}
+    </Card>
   );
 }
